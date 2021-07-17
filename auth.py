72ac4d99ad2c931af54ca4b0f6cecdcbec4cb5e2
TheoRacherR <theo.racher-raulin@outlook.com>
2021-07-17 02:29:56 +0200

    Final push

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-api_key = 'R3xRGpO1yFLT11v9d2N26Qpfz'
-api_secret_key = 'NGA2Quoaa0PCE6qcRFwusrKzorrX5lom7VJwvAFZs1vyytUOKU'
-access_token = '1413199429004414977-v4Ez8DqS24RuVaXMMKQUeo2OxUWBLn'
-access_secret_token = 'ytE1BKs2lNRhxkE7oAl8nbVoJZ7gz6DvXMp6VHfVVb4pL'
+api_key = ''
+api_secret_key = ''
+access_token = ''
+access_secret_token = ''
